98d6eb74d08aed91892aeefad2227f5b32d99c31
Parfait NM <mbamu.developer@gmail.com>
2022-04-03 19:06:46 -0400

    navbar menu

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <the-header></the-header>
+  <router-view></router-view>
 </template>
 
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader
+  }
+}
+</script>
+
+
 <style>
-@import './assets/base.css';
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+* {
+  box-sizing: border-box;
 }
 
-header {
-  line-height: 1.5;
+html {
+  font-family: "Roboto", sans-serif;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+body {
+  margin: 0;
 }
 </style>
---
+++ src/pages/mentores/MentoresList.vue
@@ -0,0 +1,10 @@
+<template>
+    <section>
+        FILTER
+    </section>
+
+    <section>
+        LIST OF MENTORES
+    </section>
+
+</template>
